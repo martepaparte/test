81eb60cf663207305c083b5e8f349a35f6867d21
martepaparte <m.dadurkeviciute@gmail.com>
2023-09-08 18:17:19 +0300

    completed task to add digits of two digits number

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def print_test():
     input_number = input("Please input two digit number: ")
     try:
         input_number = int(input_number)
-    except Exception as e:
-        print("Failed to input number: " + e)
-    try:
         if len(str(input_number)) != 2:
             raise ValueError("not two digits")
     except ValueError as e:
         print("Invalid input: " + str(e))
         return
+
+    answer = int(str(input_number)[0]) + int(str(input_number)[1])
+    print(answer)
 
 if __name__ == '__main__':
     print_test()
